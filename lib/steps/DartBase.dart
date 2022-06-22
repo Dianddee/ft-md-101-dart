aab9c0a709f8d54fa57b23e705c6ec77c02b533d
Ruslan <89350484+Dianddee@users.noreply.github.com>
2022-06-22 22:25:31 +0300

    Урок №8 - Уровни методов в dart

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -1,6 +1,7 @@
 import 'package:ft_md_101_dart/Classinstance.dart';
 import 'package:ft_md_101_dart/steps/utils/DartMethod.dart';
 import 'package:ft_md_101_dart/steps/utils/DartPackage.dart';
+import 'package:ft_md_101_dart/steps/utils/DartSignature.dart';
 
 
 class DartBase{
@@ -12,6 +13,7 @@ class DartBase{
     _stepClassInstance();
     _stepPackage();
     _stepMethod();
+    _stepSignature();
   }
   void _stepConf(){
     print('_stepConf: This is step 1');
@@ -47,5 +49,15 @@ class DartBase{
   void _stepMethod(){
     var dn = DartMethod();
     dn.run();
+  }
+
+  void _stepSignature(){
+    var ds = DartSignature('first var', "second var");
+    ds.run();
+    ds.setAndRun("first after set", 'second after set');
+
+    ds.first = 'first after set';
+    ds.second = 'second after set';
+    ds.run();
   }
 }
