186344ac286b4e8c024b9a924b75f3c06bd01910
Ruslan <89350484+Dianddee@users.noreply.github.com>
2022-06-23 22:22:15 +0300

    Урок №17 - Интерфейсы

---
+++ lib/steps/utils/DartInterfaceChild.dart
@@ -0,0 +1,7 @@
+import 'package:ft_md_101_dart/steps/utils/DartInterfaceParent.dart';
+
+class DartInterfaceChild implements DartInterfaceParent{
+  @override
+  void printMe() {
+    print('DartInterfaceChild: hi!');
+  }}
